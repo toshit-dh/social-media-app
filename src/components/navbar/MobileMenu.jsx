a82cc767f0c355ba1df4c66f5b5818dd8c2fbf61
toshit-dh <extratdh@gmail.com>
2024-11-23 15:09:38 +0530

    Buttons added

--- src/components/navbar/MobileMenu.jsx
+++ src/components/navbar/MobileMenu.jsx
@@ -6,14 +6,14 @@ export default function MobileMenu() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div>
-      <div className="">
+      <div className="md:hidden">
         <div
           className="flex flex-col gap-[4.5px] cursor-pointer"
           onClick={() => setIsOpen((e) => !e)}
         >
-          <div className="w-6 h-1 bg-orange-500 rounded-sm" />
-          <div className="w-6 h-1 bg-orange-500 rounded-sm" />
-          <div className="w-6 h-1 bg-orange-500 rounded-sm" />
+          <div className={`w-6 h-1 bg-orange-500 rounded-sm ${isOpen ? "rotate-45" : ""} origin-left ease-in-out duration-500`} />
+          <div className={`w-6 h-1 bg-orange-500 rounded-sm ${isOpen ? "opacity-0" : ""} ease-in-out duration-500`} />
+          <div className={`w-6 h-1 bg-orange-500 rounded-sm ${isOpen ? "-rotate-45" : ""} origin-left ease-in-out duration-500`} />
         </div>
         {isOpen && (
           <div className="absolute left-0 top-24 w-full h-[calc(100vh - 96px)] bg-white flex flex-col items-center justify-center gap-8 font-medium text-xl z-10">
